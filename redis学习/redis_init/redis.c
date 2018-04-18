dd7e1c30a62fb32260c643a094f44cd61d3a7189
Mr.Cai <499411466@qq.com>
2018-04-18 17:57:19 +0800

    init

--- redis学习/redis_init/redis.c
+++ redis学习/redis_init/redis.c
@@ -2,8 +2,38 @@
 
 struct redisServer server;
 
-void initServerConfig(){
+struct redisCommand redisCommandTable[] = {
+	{ "get", getCommand, 2, "r", 0, NULL, 1, 1, 1, 0, 0},
+	{ "set", setCommand, -3, "wm", 0, NULL, 1, 1, 1, 0, 0},
+};
 
+void initServerConfig(){
+	int j;
+
+	//服务器状态
+	server.hz = REDIS_DEFAULT_HZ;
+	server.port = REDIS_SERVERPORT;
+	server.tcp_backlog = REDIS_TCP_BACKLOG;
+	server.bindaddr_count = 0;
+
+	server.maxclients = REDIS_MAX_CLIENTS;
+	server.ipfd_count = 0;
+	server.dbnum = REDIS_DEFAULT_DBNUM;
+	server.tcpkeepalive = REDIS_DEFAULT_TCP_KEEPALIVE;
+
+
+}
+
+/*================================== Commands ================================ */
+
+/*
+ * 根据 redis.c 文件顶部的命令列表，创建命令表
+ */
+void populateCommandTable(void){
+	int j;
+
+	// 命令的数量
+	int num
 }
 
 int main(int argc, char *argv[]){
--- redis学习/redis_init/redis.h
+++ redis学习/redis_init/redis.h
@@ -3,8 +3,37 @@
 
 #include <stdio.h>
 #include <dict.h>
+#include "ea.h"
+#include "adlist.h"
 
-typedf struct redisDb
+/*协议和I/O相关的定义*/
+#define REDIS_REPLY_CHUNK_BYTES	(16*1024) /* 16k 输出缓存*/
+#define REDIS_MAX_CLIENTS 10000 /* 最大所支持的用户数目 */
+
+/*服务器配置*/
+#define REDIS_DEFAULT_HZ        10
+#define REDIS_SERVERPORT		6379 /* TCP port */
+#define REDIS_TCP_BACKLOG       511     /* TCP listen backlog */ 
+#define REDIS_BINDADDR_MAX		16
+#define REDIS_IP_STR_LEN INET6_ADDRSTRLEN
+#define REDIS_DEFAULT_DBNUM     16
+#define REDIS_DEFAULT_TCP_KEEPALIVE 0
+
+#define REDIS_LRU_BITS 24
+
+typedef struct redisObject{
+	unsigned type : 4;	// 类型
+
+	unsigned encoding : 4; //编码
+
+	unsigned lru : REDIS_LRU_BITS; // 对象最后一次被访问的时间
+
+	int refcount;	// 引用计数
+ 
+	void *ptr;		// 指向实际值的指针
+}robj;
+
+typedef struct redisDb
 {
 	dict *dict;			// 数据库键空间，保存着数据库中的所有键值对
 
@@ -15,7 +44,43 @@ typedf struct redisDb
 	dict *ready_keys;	// 可以解除阻塞的键
 
 	int id;				// 数据库号码
-};
+}redisDb;
+
+
+typedef struct redisClient
+{
+	int fd;		// 套接字描述符
+
+	redisDb *db; // 当前正在使用的数据库
+
+	int dictid;	// 当前正在使用的数据库id
+
+	robj *name; // 客户端的名字
+
+	sds querbuf; // 查询缓冲区
+
+	size_t querbuf_peak; // 查询缓冲区长度峰值
+
+	int argc; // 参数总量
+
+	robj ** argc; // 参数对象数组
+
+	strcut redisCommand * cmd, *lastcmd;	// 记录被客户执行的命令
+
+	int reqtype;	// 请求类型，是内联命令还是多条命令
+
+	int multibulklen;	//剩余未读去的命令内容数量
+
+	long bulklen; // 命令内容的长度
+
+	list *reply;	// 回复链表
+
+	unsigned long reply_bytes;	//回复链表中对象的总大小
+
+	int bufpos;	//回复偏移量
+
+	char buf[REDIS_RE]
+}redisClient;
 
 struct redisServer{
 
@@ -23,7 +88,65 @@ struct redisServer{
 
 	int hz;				// serverCron()每秒调用的次数
 
-	redisDb *db;		//一个数组，保存着服务器
+	redisDb *db;		// 一个数组，保存着服务器
+
+	dict *commands;		// 命令表（受到rename配置选项的作用）
+
+	aeEventLoop *el;	// 事件状态
+
+	int shutdown_asap;	// 关闭服务器的标识
+
+	int port;			// TCP 监听端口
+
+	int tcp_backlog;	// TCP listen() backlog
+
+	char *bindaddr[REDIS_BINDADDR_MAX];  // ip 地址
+
+	int bindaddr_count; // 地址的数量
+
+	int ipfd[REDIS_BINDADDR_MAX]; 		 // TCP 描述符
+
+	int ipfd_count;		// 已使用了的描述符的数目
+
+	list *clients;		// 一个链表，保存了所有的客户端状态结构
+
+	list *clients_to_close; // 保存了所有待关闭的客户端
+
+	redisClient *current_client; // 服务器当前服务的客户端，仅用于崩溃报告
+
+	char neterr[ANET_ERR_LEN];	// 用于记录网络错误
+
+	int tcpkeepalive;	// 是否开启 SO_KEEPALIVE选项
+	int dbnum;			// 数据库的总数目
+
+	int maxclients;		//最大并发数量
 };
 
+
+typedef void redisCommandProc(redisClient *c);
+typedef int *redisGetKeyProc(struct redisCommand *cmd, robj **argv, int argc, int *numkeys);
+/*
+ * redisCommand redis命令
+ */
+struct redisCommand{
+	
+	char *name;		// 命令名称
+
+	redisCommandProc *proc;		// 实现函数
+
+	int arity;		// 参数个数
+
+	char *sflags;	//字符串表示的FLAG
+
+	redisGetKeyProc *getkeys_proc;	//从命令中判断命令的键参数。在redis集群转向时使用。
+
+	int firstkey;
+	int lastkey;
+	int keystep;
+
+	// 统计信息
+	// microseconds 记录了命令执行耗费的总毫微秒数
+	// calls 是命令被执行的总次数
+	long long microseconds, calls;
+};
 #endif
